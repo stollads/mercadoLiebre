dac8124222a050b583fa8108939b0ec15d144efd
StollARG <stoll.ads@gmail.com>
2022-06-13 18:57:53 -0300

    entrega_mLibre

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+
+
+
+app.use(express.static(publicPath));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
+app.post('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
+
+
+app.listen(process.env.PORT || 3000, () => console.log("Servidor corriendo en Puerto: 3000"));
+
+
